f10c770ccf4c9c160c29e173ab2b4911f606a2a0
Philipp Stucki <ps@stuckistucki.com>
2022-03-24 23:22:08 +0100

    adds instr+arg compilation

--- src/compiler.rs
+++ src/compiler.rs
@@ -19,7 +19,7 @@ fn is_digit(c: char) -> bool {
 }
 
 fn is_whitespace(c: char) -> bool {
-    c == ' ' || c == '\t'
+    c == ' ' || c == '\t' || c == '\n'
 }
 
 fn requires_argument(instruction: &str) -> Result<bool> {
@@ -36,9 +36,9 @@ pub struct Compiler<'a> {
     state: ParserState,
     labels: HashMap<String, u16>,
     output: Vec<Opcode>,
-    currentToken: Vec<char>,
-    currentCharacter: Option<char>,
-    currentInstruction: Option<String>,
+    current_token: Vec<char>,
+    current_character: Option<char>,
+    current_instruction: Option<String>,
 }
 
 impl<'a> Compiler<'a> {
@@ -48,14 +48,14 @@ impl<'a> Compiler<'a> {
             state: ParserState::LabelOrInstruction,
             labels: HashMap::new(),
             output: vec![],
-            currentToken: vec![],
-            currentCharacter: None,
-            currentInstruction: None,
+            current_token: vec![],
+            current_character: None,
+            current_instruction: None,
         }
     }
 
     fn read(&mut self) {
-        self.currentCharacter = self.iit.next();
+        self.current_character = self.iit.next();
     }
 
     fn read_while<P>(&mut self, pred: P)
@@ -67,7 +67,7 @@ impl<'a> Compiler<'a> {
                 None => break,
                 Some(c) => {
                     if pred(*c) {
-                        self.currentToken.push(*c);
+                        self.current_token.push(*c);
                         // advance iterator if peek was successful
                         self.iit.next();
                     } else {
@@ -84,6 +84,18 @@ impl<'a> Compiler<'a> {
         argument: Option<StackType>,
     ) -> Result<()> {
         match instruction {
+            "push" => Ok(self.output.push(Opcode::PushConstant(argument.unwrap()))),
+            "dup" => Ok(self.output.push(Opcode::Dup(argument.unwrap() as u16))),
+            "bnz" => Ok(self
+                .output
+                .push(Opcode::BranchIfNotZero(argument.unwrap() as u16))),
+
+            "drop" => Ok(self.output.push(Opcode::Swap)),
+            "swp" => Ok(self.output.push(Opcode::Swap)),
+            "add" => Ok(self.output.push(Opcode::Add)),
+            "sub" => Ok(self.output.push(Opcode::Subtract)),
+            "mul" => Ok(self.output.push(Opcode::Multiply)),
+
             _ => bail!("unknown instruction {}", instruction),
         }
         // self.output.push(instruction_meta.opcode);
@@ -94,13 +106,13 @@ impl<'a> Compiler<'a> {
             match self.state {
                 ParserState::LabelOrInstruction => {
                     self.read_while(is_whitespace);
-                    self.currentToken = vec![];
+                    self.current_token = vec![];
 
                     self.read_while(is_letter);
 
                     let c = self.iit.peek();
 
-                    let token = String::from_iter(&self.currentToken);
+                    let token = String::from_iter(&self.current_token);
                     println!("label or instr: {:?}, p: {:?}", token, c);
 
                     match c {
@@ -110,28 +122,35 @@ impl<'a> Compiler<'a> {
                             self.labels.insert(token, self.output.len() as u16);
                             self.state = ParserState::Instruction;
                         }
-                        _ => {
+                        _ if token.len() > 0 => {
                             // is an instruction
                             if requires_argument(&token).unwrap() {
-                                self.currentInstruction = Some(String::from(token));
+                                self.current_instruction = Some(String::from(token));
                                 self.state = ParserState::Argument;
                             } else {
                                 self.compile_instruction(&token, None).unwrap();
                                 self.state = ParserState::LabelOrInstruction;
-                                self.currentInstruction = None;
+                                self.current_instruction = None;
                             }
                         }
+                        _ => {} // ignore empty tokens
                     }
                 }
 
                 ParserState::Argument => {
                     self.read_while(is_whitespace);
 
-                    self.currentToken = vec![];
+                    self.current_token = vec![];
                     self.read_while(is_digit);
 
-                    if let Ok(arg) = String::from_iter(&self.currentToken).parse::<i16>() {
+                    if let Ok(arg) = String::from_iter(&self.current_token).parse::<i16>() {
                         println!("argument: {:?}", arg);
+
+                        self.compile_instruction(
+                            &(self.current_instruction.clone()).unwrap(),
+                            Some(arg),
+                        )
+                        .unwrap();
                     }
                 }
 
@@ -158,7 +177,14 @@ mod tests {
 
     #[test]
     fn test2() {
-        let mut c = Compiler::new("  push 12");
+        let mut c = Compiler::new("  push   12");
+        c.compile();
+        println!("output: {:#?}", c);
+    }
+
+    #[test]
+    fn test3() {
+        let mut c = Compiler::new("  add\n  sub\n");
         c.compile();
         println!("output: {:#?}", c);
     }
